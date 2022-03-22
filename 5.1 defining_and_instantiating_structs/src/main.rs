7ec8ae2e60ed00d0fd135551188fbf1ece9831e3
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-03-22 17:10:43 +0530

    ⚔️ Quest: Structs

--- 5.1 defining_and_instantiating_structs/src/main.rs
+++ 5.1 defining_and_instantiating_structs/src/main.rs
@@ -23,52 +23,65 @@ struct Point(i32, i32, i32);
 // Unit-like structs without any fields
 struct AlwaysEqual;
 
+// ⚔️ Quest
+struct Userdetails {
+    firstname: String,
+    lastname: String,
+}
+
 fn main() {
+    // ⚔️ Quest
+    let user1 = Userdetails {
+        firstname: String::from("Hitesh"),
+        lastname: String::from("Kumar"),
+    };
+    println!("My full name is {} {}", user1.firstname, user1.lastname);
+
     // User struct instantiation
-    let mut user1 = User {
-        email: String::from("user1@mail.com"),
-        username: String::from("username1"),
+    let mut user2 = User {
+        email: String::from("user2@mail.com"),
+        username: String::from("username2"),
         active: true,
         sign_in_count: 1,
     };
 
     // Changing the value of a field needs whole struct to be mutable
-    user1.email = String::from("user1_new@mail.com");
-    println!("User 1's active status: {}", user1.active);
-    println!("User 1's username: {}", user1.username);
-    println!("User 1's email: {}", user1.email);
-    println!("User 1's sign_in_count: {}", user1.sign_in_count);
-
-    // Using function to create instance of User struct
-    let user2 = build_user(String::from("user2@mail.com"), String::from("username2"));
+    user2.email = String::from("user2_new@mail.com");
     println!("User 2's active status: {}", user2.active);
     println!("User 2's username: {}", user2.username);
     println!("User 2's email: {}", user2.email);
     println!("User 2's sign_in_count: {}", user2.sign_in_count);
 
-    // Creating instances from other instances
-    let user3 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("user3@mail.com"),
-        sign_in_count: user1.sign_in_count,
-    };
-
+    // Using function to create instance of User struct
+    let user3 = build_user(String::from("user3@mail.com"), String::from("username3"));
     println!("User 3's active status: {}", user3.active);
     println!("User 3's username: {}", user3.username);
     println!("User 3's email: {}", user3.email);
     println!("User 3's sign_in_count: {}", user3.sign_in_count);
 
-    // Creating instances from other instances with struct update syntax
+    // Creating instances from other instances
     let user4 = User {
-        email: String::from("user3@mail.com"),
-        ..user2
+        active: user2.active,
+        username: user2.username,
+        email: String::from("user4@mail.com"),
+        sign_in_count: user2.sign_in_count,
     };
 
     println!("User 4's active status: {}", user4.active);
     println!("User 4's username: {}", user4.username);
     println!("User 4's email: {}", user4.email);
     println!("User 4's sign_in_count: {}", user4.sign_in_count);
+
+    // Creating instances from other instances with struct update syntax
+    let user5 = User {
+        email: String::from("user5@mail.com"),
+        ..user3
+    };
+
+    println!("User 5's active status: {}", user5.active);
+    println!("User 5's username: {}", user5.username);
+    println!("User 5's email: {}", user5.email);
+    println!("User 5's sign_in_count: {}", user5.sign_in_count);
 
     // black and origin values are of different types
     let black = Color(0, 0, 0);
